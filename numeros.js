f063f2ae87d93d25f27574ccb6f30581d87901e4
RicardoCano35 <you@example.com>
2024-11-07 11:11:29 -0500

    Revert "números del 1 al 50"
    This reverts commit 8fb3ed5665de0952616d520270d1be6effed2420.

--- numeros.js
+++ numeros.js
@@ -1,15 +1,6 @@
-// for (let numero = 1; numero <= 50; numero++) {
-//     console.log(numero);
-// }
+// Script para mostrar los números del 1 al 50 en la consola
 
-
-// for (let numero = 1; numero <= 2000; numero++) {
-//     console.log(numero);
-// }
-
-for (let numero = 1; numero <= 2000; numero++) {
-    let numeroPorCadena = numero.toString();
-    if (numeroPorCadena.startsWith('2')) {
-        console.log(numero);
-    }
+for (let numero = 1; numero <= 50; numero++) {
+    console.log(numero);
 }
+
