c43f5985caf30e44261eff84e5ca0265a9b99249
jupardu <jupardu@gmail.com>
2024-11-11 22:00:44 -0500

    Tarea 3 numeros que empiezan con 2

--- JUAN-PABLO-ARISTIZABAL-3122969524/CLASE3/main.js
+++ JUAN-PABLO-ARISTIZABAL-3122969524/CLASE3/main.js
@@ -1,4 +1,7 @@
-console.log('Hola Mundo')
- for (i=1 ; i <= 2000; i++){
-     console.log('${i}')
+for (i=1 ; i <= 2000; i++){
+    if(i.toString().startsWith('2')){
+        console.log(i)
+       }
  }
+
+ 
