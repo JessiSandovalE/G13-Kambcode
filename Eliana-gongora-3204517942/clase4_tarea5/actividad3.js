81ccd68412520710814307fde661a279a30c3d5b
Eliana <elypgongora@gmail.com>
2024-11-19 20:53:55 -0500

    subo tarea 5

---
+++ Eliana-gongora-3204517942/clase4_tarea5/actividad3.js
@@ -0,0 +1,12 @@
+true || true || false && true || false && true && true || false
+//expresion:
+!false && (false || false && false || (true && true)) || false && true
+
+
+true && (false || false && (true && (true || false && true))) || false && (true || false)
+//expresion:
+!false && true || false && true
+
+!(!true && (false || false && !!!true || (true && !false))) || false && true
+//expresion:
+true || false
